0285344da2c719e4d8ac7496e616c5205f9ceac4
LILSYNNOFFICIAL <lilsynn89@gmail.com>
2026-06-11 18:23:37 -0700

    Add app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+class SynapseApp:
+    def __init__(self):
+        self.name = 'Project Synapse'
+        self.version = '0.1.0'
+
+    def run(self):
+        print(f'{self.name} v{self.version} initialized.')
